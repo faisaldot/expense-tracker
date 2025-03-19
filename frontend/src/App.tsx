f935b06ced9cf1ee0caed2b46c41f934101df4a7
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2025-03-19 16:16:12 +0600

    setup vite proxy server and make http request from backend to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,23 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [totalSpent, setTotalSpent] = useState(0);
+
+  useEffect(() => {
+    const fetchTotalSpent = async () => {
+      const response = await fetch("api/expenses/total-spent");
+      const data = await response.json();
+      console.log(data);
+
+      setTotalSpent(data.total);
+    };
+
+    fetchTotalSpent();
+  }, []);
+
   return (
     <div className="flex flex-col items-center m-10">
       <Card className="w-lg text-center">
@@ -17,7 +30,7 @@ export default function App() {
           <CardDescription>Your total amount of spent</CardDescription>
         </CardHeader>
         <CardContent>
-          <p className="text-4xl">{totalSpent}৳</p>
+          <p className="text-4xl">{totalSpent}</p>
         </CardContent>
       </Card>
     </div>
