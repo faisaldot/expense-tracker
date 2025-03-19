86b542b4f2202c8361a5ccc33c1a9e0727235645
Faisal-Mahmud-Anik <bandafaisalmahmud@gmail.com>
2025-03-19 15:59:38 +0600

    added a card component for total spent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useState } from "react";
+
 export default function App() {
+  const [totalSpent, setTotalSpent] = useState(0);
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <div className="flex flex-col items-center m-10">
+      <Card className="w-lg text-center">
+        <CardHeader>
+          <CardTitle className="text-2xl">Total Spent</CardTitle>
+          <CardDescription>Your total amount of spent</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="text-4xl">{totalSpent}৳</p>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
